e231f70a10f00a7b16def5f19b088a21e87033f8
Henry Zai <hjuniorzai@gmail.com>
2023-12-04 15:34:29 +0700

    create layout login activity

---
+++ app/src/main/java/com/example/bangkit_capstone/ui/auth/login/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.example.bangkit_capstone.ui.auth.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bangkit_capstone.R
+import com.example.bangkit_capstone.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
