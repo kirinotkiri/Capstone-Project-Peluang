6d1d1a884d073443dbb4f33df76e2c85b1fd7467
Henry Zai <hjuniorzai@gmail.com>
2023-12-14 15:19:24 +0700

    create class Injection and viewModelFactory

---
+++ app/src/main/java/com/example/bangkit_capstone/ui/ViewModelFactory.kt
@@ -0,0 +1,29 @@
+package com.example.bangkit_capstone.ui
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.bangkit_capstone.di.Injection
+import com.example.bangkit_capstone.repository.Repository
+import com.example.bangkit_capstone.ui.auth.SignUpViewModel
+
+class ViewModelFactory (private val repository: Repository) : ViewModelProvider.NewInstanceFactory() {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return when {
+            modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
+                SignUpViewModel(repository) as T
+            }
+            else -> throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var instance: ViewModelFactory? = null
+        fun getInstance(context: Context): ViewModelFactory =
+            instance ?: synchronized(this) {
+                instance ?: ViewModelFactory(Injection.provideRepository(context))
+            }.also { instance = it }
+    }
+}
