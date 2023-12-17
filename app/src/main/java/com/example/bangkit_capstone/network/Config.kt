fc9dbe82f46cecc3db7156b7bd7e8e245d6a5eec
kirinotkiri <133612937+kirinotkiri@users.noreply.github.com>
2023-12-17 22:12:13 +0700

    Config changes

---
+++ app/src/main/java/com/example/bangkit_capstone/network/Config.kt
@@ -0,0 +1,35 @@
+package com.example.bangkit_capstone.network
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Config {
+    fun getApiService(token: String, endpoint: String): ApiService {
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val authInterceptor = Interceptor {
+            val req = it.request()
+            val requestHeader = req.newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            it.proceed(requestHeader)
+        }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(authInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(endpoint)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
+}
