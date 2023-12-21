ca85fde9511d5ecb0b025d154c2e56832b08c601
kirinotkiri <133612937+kirinotkiri@users.noreply.github.com>
2023-12-21 17:37:16 +0700

    Fixes

--- app/src/main/java/com/example/bangkit_capstone/atlaunch/LoginOrRegisterActivity.kt
+++ app/src/main/java/com/example/bangkit_capstone/atlaunch/LoginOrRegisterActivity.kt
@@ -1,16 +1,21 @@
 package com.example.bangkit_capstone.atlaunch
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bangkit_capstone.R
 import com.example.bangkit_capstone.databinding.ActivityLoginOrRegisterBinding
 import com.example.bangkit_capstone.di.Injection
+import com.example.bangkit_capstone.di.LoginHandler
 import com.example.bangkit_capstone.home.HomeActivity
 import com.example.bangkit_capstone.ui.auth.login.LoginActivity
 import kotlinx.coroutines.runBlocking
 
 class LoginOrRegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginOrRegisterBinding
+    private lateinit var logon: LoginHandler
     override fun onCreate(savedInstanceState: Bundle?) {
         /*
         for some reason the splash screen doesn't set the theme back after launch, so this piece of code is
@@ -20,25 +25,58 @@ class LoginOrRegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_or_register)
 
-        val binding = ActivityLoginOrRegisterBinding.inflate(layoutInflater)
+        binding = ActivityLoginOrRegisterBinding.inflate(layoutInflater)
 
-        val logon = Injection.loginProvider(this@LoginOrRegisterActivity)
+        logon = Injection.loginProvider(this@LoginOrRegisterActivity)
+        Log.d("LOGINORREGISTER", "onCreate: TEST")
 
-        binding.apply{
-            btnLoginOrSignup.setOnClickListener {
+        /*
+        binding.btnLoginOrSignup.setOnClickListener {
+            Log.d("LOGINORREGISTER", "onCreate: to login activity")
+
+            val intent = Intent(this@LoginOrRegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+            runBlocking {
+                logon.setNotNew()
+            }
+            Log.d("LOGINORREGISTER", "onCreate: to login activity")
+            finish()
+
+        }
+
+        binding.btnToHome.setOnClickListener {
+            Log.d("LOGINORREGISTER", "onCreate: to home")
+
+            val intent = Intent(this@LoginOrRegisterActivity, HomeActivity::class.java)
+            startActivity(intent)
+            runBlocking {
+                logon.setNotNew()
+            }
+            Log.d("LOGINORREGISTER", "onCreate: to home")
+
+            finish()
+        }
+         *///this is absolutely dumb but for some reason setOnClickListener refuses to work, WHY?
+    }
+
+    fun onClickedHere(view: View){
+        when(view.id){
+            R.id.btnLoginOrSignup ->{
                 val intent = Intent(this@LoginOrRegisterActivity, LoginActivity::class.java)
                 startActivity(intent)
-                runBlocking{
+                runBlocking {
                     logon.setNotNew()
-                    this@LoginOrRegisterActivity.finish()}
-
+                }
+                finish()
             }
-            btnToHome.setOnClickListener {
+
+            R.id.btnToHome ->{
                 val intent = Intent(this@LoginOrRegisterActivity, HomeActivity::class.java)
                 startActivity(intent)
-                runBlocking{
+                runBlocking {
                     logon.setNotNew()
-                    this@LoginOrRegisterActivity.finish()}
+                }
+                finish()
             }
         }
     }
